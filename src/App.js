2dcaaf1bc379aaf3dbea35422ef39fc50dac3b63
Maria M <prov21_1@hotmail.com>
2021-06-06 10:52:15 -0700

    nav component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import About from "./components/About";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
