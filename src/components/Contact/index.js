03ae2316c0cb9e23f74822541e2bc3e8971840e6
Maria M <prov21_1@hotmail.com>
2021-06-13 11:31:02 -0700

    handle contact form submit:

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -17,10 +17,15 @@ function ContactForm() {
   }
 
   //console.log(formState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input
